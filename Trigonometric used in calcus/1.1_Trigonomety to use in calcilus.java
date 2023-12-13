deb85a22b9d51cddf61074d83314083eb9bbbd7a
TenexDesigns <117345133+TenexDesigns@users.noreply.github.com>
2023-12-14 00:18:01 +0300

    Create 1.1_Trigonomety to use in calcilus.java

---
+++ Trigonometric used in calcus/1.1_Trigonomety to use in calcilus.java
@@ -0,0 +1,22 @@
+Trigonometry plays a crucial role in calculus, especially in the study of functions, limits, derivatives, and integrals. Here are some of the key areas where trigonometry is used in calculus:
+
+1. **Trigonometric Functions**: The basic trigonometric functions such as sine, cosine, and tangent are used extensively in calculus. For instance, the derivatives of these functions are used in the study of limits and continuity. Similarly, integrals of these functions are used in the study of areas under curves [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+2. **Trigonometric Identities**: These are fundamental relationships between trigonometric functions and are used in various proofs and simplifications in calculus. Some of the most important identities include the Pythagorean identity, double-angle formulae, and identities of angle addition and subtraction [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+3. **Inverse Trigonometric Functions**: These functions are used in calculus to solve equations involving trigonometric functions. They are particularly useful in the study of limits and derivatives [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+4. **Hyperbolic Functions**: These are extensions of the trigonometric functions to the hyperbola. They are used in calculus in the study of exponential growth and decay, and in the study of complex numbers [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+5. **Trigonometric Integrals**: These are integrals involving trigonometric functions. They are used in the study of areas under curves, and in the solution of differential equations [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+6. **Unit Circle**: The unit circle is a circle of radius 1. It is used to define the trigonometric functions and to understand the properties of these functions. It is also used in the study of complex numbers [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+7. **Law of Sines and Cosines**: These laws are used in calculus to solve problems involving triangles. They are particularly useful in the study of limits and continuity [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+In terms of which formulas are most relevant, the ones you need to know depend on the specific topics in calculus you are studying. However, some of the most important ones include the derivatives of the basic trigonometric functions, the double-angle formulae, the identities of angle addition and subtraction, and the formulas for the inverse trigonometric functions. These formulas are fundamental to understanding the behavior of these functions and to solving problems involving these functions in calculus [Source 4](https://en.wikibooks.org/wiki/Calculus/Trigonometry).
+
+
+
+
+...
